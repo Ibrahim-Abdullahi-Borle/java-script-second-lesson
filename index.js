b33e48cc7e6353415ef074aa1360c08f87d15d4c
Ibrahim-Abdullahi-Borle <ibroabdullahi254@gmail.com>
2025-05-06 02:20:11 -0700

    java-script-second-lesson

---
+++ index.js
@@ -0,0 +1,23 @@
+var message= "hello";
+var name= "ibro";
+
+console.log(message +" "  +name);
+
+var school = "garissa";
+console.log(school.length);
+
+var library = "books";
+console.log(library.length);
+
+var  food=  "pizza";
+console.log(food.slice(0,3));
+var private = "car";
+console.log(private.toUpperCase())
+console.log(private.toLowerCase())
+
+
+
+
+
+
+
